cb432ae3c8eab9599f70685bdc9b01552dac16a8
Robbert Brandsma <i@robbertbrandsma.nl>
2017-08-11 15:36:56 +0200

    More tests

--- Tests/SchrodingerTests/SchrodingerTests.swift
+++ Tests/SchrodingerTests/SchrodingerTests.swift
@@ -2,12 +2,27 @@ import XCTest
 import Foundation
 @testable import Schrodinger
 
+fileprivate enum FooError : Error {
+    case bar
+}
+
 class SchrodingerTests: XCTestCase {
     
-    func testFutureAwait() throws {
+    func testDirectlyCompletedFutureAwait() throws {
         let value = 24
         
         let future = Future {
+            return value
+        }
+        
+        XCTAssertEqual(try future.await(for: .seconds(1)), value)
+    }
+    
+    func testDelayedCompletionFutureAwait() throws {
+        let value = 24
+        
+        let future = Future<Int> {
+            Thread.sleep(forTimeInterval: 0.5)
             return value
         }
         
@@ -20,6 +35,40 @@ class SchrodingerTests: XCTestCase {
         let thenExpectation = expectation(description: "The then closure is called")
         future.then {
             thenExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testFutureIsCompleted() throws {
+        let future = Future { Thread.sleep(forTimeInterval: 0.5) }
+        XCTAssertFalse(future.isCompleted)
+        try future.await(for: .seconds(1))
+        XCTAssertTrue(future.isCompleted)
+    }
+    
+    func testDirectlyErroringFutureCatch() throws {
+        let future = Future { throw FooError.bar }
+        
+        let errorExpectation = expectation(description: "The catch closure is called")
+        future.catch { error in
+            XCTAssertEqual(error as? FooError, FooError.bar)
+            errorExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testDelayedErroringFutureCatch() throws {
+        let future = Future<Void> {
+            Thread.sleep(forTimeInterval: 0.5)
+            throw FooError.bar
+        }
+        
+        let errorExpectation = expectation(description: "The catch closure is called")
+        future.catch { error in
+            XCTAssertEqual(error as? FooError, FooError.bar)
+            errorExpectation.fulfill()
         }
         
         waitForExpectations(timeout: 1)
