52af0ab6dc779931fc204d10bd9a891206b53080
Joannis Orlandos <joannis@orlandos.nl>
2019-08-06 13:47:51 +0200

    Swift 5 support

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
 // swift-tools-version:4.0
-
 import PackageDescription
 
-let package = Package(
-    name: "Schrodinger"
+var package = Package(
+    name: "Schrodinger",
+    products: [
+        .library(name: "Schrodinger", targets: ["Schrodinger"])
+    ],
+    targets: [
+        .target(name: "Schrodinger")
+    ]
 )
